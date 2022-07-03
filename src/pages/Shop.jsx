76492bc69a55ef43ce61988f4400ce741091b6e4
Eric Kong <erickongdata@gmail.com>
2022-07-03 18:08:21 +0100

    Refactor ShopItemGallery component to jsx file

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,12 +1,10 @@
 import { useContext } from 'react';
-import { Link } from 'react-router-dom';
 import { AppContext } from '../AppContext';
-import toGBP from '../utilities/formatCurrency';
-import { filterData } from '../utilities/categoryFilter';
 import ShopSortingButtons from '../components/ShopSortingButtons';
+import ShopItemGallery from '../components/ShopItemGallery';
 
 function Shop() {
-  const { data, category, sorting } = useContext(AppContext);
+  const { category } = useContext(AppContext);
 
   return (
     <div className="shop">
@@ -16,40 +14,7 @@ function Shop() {
             <ShopSortingButtons />
           </div>
           <h1 className="text-capitalize">{category}</h1>
-          <div className="row gy-4 gx-4">
-            {filterData(data, category, sorting.alpha, sorting.price).map(
-              (item) => (
-                <div
-                  key={item.id}
-                  className="col-12 col-md-6 col-lg-4 col-xlg-3"
-                >
-                  <Link
-                    to={`/product/${item.id}`}
-                    className="text-decoration-none text-dark"
-                  >
-                    <div className="card" style={{ height: '280px' }}>
-                      <img
-                        src={item.src}
-                        alt={item.title}
-                        className="card-img-top"
-                        style={{
-                          objectFit: 'cover',
-                          height: '200px',
-                        }}
-                      />
-                      <div
-                        className="d-flex justify-content-between gx-5 p-1"
-                        style={{ height: '80px' }}
-                      >
-                        <h6 className="card-title pe-4">{item.title}</h6>
-                        <p>{toGBP(item.price)}</p>
-                      </div>
-                    </div>
-                  </Link>
-                </div>
-              )
-            )}
-          </div>
+          <ShopItemGallery />
         </div>
       </div>
     </div>
