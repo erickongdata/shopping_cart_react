39a9ef11d78fe1146140d6001cf4c118a029e605
Eric Kong <erickongdata@gmail.com>
2022-07-15 10:09:29 +0100

    Change About page and doc title

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,8 @@ function About() {
   return (
     <div className="container">
       <div className="pt-3 pb-7">
-        <h1>About Page</h1>
+        <h1 className="display-3">Hi!</h1>
+        <h3 className="font-handlee">We are All Things Colour...</h3>
       </div>
     </div>
   );
